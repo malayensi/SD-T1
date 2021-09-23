cd60c1d8798982fa0f5368838204fad017054c5f
malequi <73211835+malequi@users.noreply.github.com>
2021-09-23 03:17:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import *
 import redis
+import cliente
 app= Flask(__name__)
 r= redis.Redis(host='localhost', port=6379, db=0)
 @app.route('/', methods=['GET', 'POST'])
@@ -18,11 +19,23 @@ def busqueda():
     producto= request.args.get('producto')
     value= r.get(producto)
     if value==None:
-        print("hola")
-        return("<h3>hola mundo</h3>")
+        try:
+            #respuestaservidor=cliente.funcion(producto)
+            texto=str(cliente.funcion("cpu"))
+            precioproducto=texto.split()[5]
+            #cliente.funcion(producto)
+            #data = jsonify(json.load(cliente.funcion(producto)))
+            #for i in data['products']:
+                #print(i['price'])
+            r.set(producto,precioproducto)
+            return(f'{cliente.funcion(producto)}')
+            #return(respuestaservidor)
+        except Exception:
+            return("<h3>producto no encontrado</h3>")
+        
     else:
         response = {"nombre producto":str(producto),
-                    "precio producto":str(value)}
+                    "precio producto":str(value).split("'")[1]}
         return jsonify(response)
     #password = request.args.get('password')
     #return username
